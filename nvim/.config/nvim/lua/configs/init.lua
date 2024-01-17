1c85ca71e1f865ca46e78c0f26a02a6935027737
Enzo Bettini <enzo@chemify.io>
2024-01-17 14:53:25 +0000

    feat: Added toggleterm
    Currently only configured toggleterm to open up lazygit

--- nvim/.config/nvim/lua/configs/init.lua
+++ nvim/.config/nvim/lua/configs/init.lua
@@ -4,7 +4,8 @@ require('configs.lsp')
 require('configs.whichkey')
 require('configs.cmp')
 require('configs.dap')
-require('configs/nvimtree')
-require('configs/treesitter')
+require('configs.nvimtree')
+require('configs.treesitter')
+require('configs.toggleterm')
 
 -- vim: ts=2 sts=2 sw=2 et
---
+++ nvim/.config/nvim/lua/plugins/toggleterm.lua
@@ -0,0 +1,4 @@
+return {
+  {'akinsho/toggleterm.nvim', version = "2.9", config = true}
+}
+-- vim: ts=2 sts=2 sw=2 et

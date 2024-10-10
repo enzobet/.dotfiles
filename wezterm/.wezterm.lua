cc4044ed21568fa539b5a86e89477c0226d2353b
Enzo Bettini <enzo@chemify.io>
2024-10-10 22:53:18 +0100

    feat: added wezterm config

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,22 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+
+config.font = wezterm.font("FiraCode Nerd Font Mono")
+
+config.enable_tab_bar = false
+
+config.window_decorations = "RESIZE"
+config.window_background_opacity = 0.95
+config.macos_window_background_blur = 10
+
+
+-- For example, changing the color scheme:
+config.color_scheme = 'Tokyo Night'
+
+-- and finally, return the configuration to wezterm
+return config

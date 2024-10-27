16fd35fd089b5ff231f7767c79b31450a1c6a59a
Enzo Bettini <enzo@chemify.io>
2024-10-27 21:29:23 +0000

    feat(nvim): added kemap to toggle bg transparency

--- nvim/.config/nvim/lua/plugins/themes/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/themes/catppuccin.lua
@@ -1,3 +1,20 @@
 return {
-    "catppuccin/nvim", name = "catppuccin", priority = 1000
+	"catppuccin/nvim",
+	name = "catppuccin",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		require("catppuccin").setup({
+			transparent_background = true,
+		})
+
+		local toggle_bg = function()
+			local cat = require("catppuccin")
+			cat.options.transparent_background = not cat.options.transparent_background
+			cat.compile()
+			vim.cmd.colorscheme(vim.g.colors_name)
+		end
+
+		vim.keymap.set("n", "<leader>bg", toggle_bg, { noremap = true, silent = true })
+	end,
 }

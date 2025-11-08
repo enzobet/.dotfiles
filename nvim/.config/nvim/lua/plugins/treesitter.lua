473ad1f4b08b556b33aec3373bc20561280db6c3
Enzo Bettini <enzo@chemify.io>
2025-11-08 16:14:43 +0000

    feat(nvim): added nvim treesitter context plugin

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,5 +1,6 @@
 -- Highlight, edit, and navigate code
 return {
+    {
 	"nvim-treesitter/nvim-treesitter",
     branch = "master",
 	build = ":TSUpdate",
@@ -96,4 +97,23 @@ return {
 		vim.filetype.add({ extension = { tf = "terraform" } })
 		vim.filetype.add({ extension = { tfvars = "terraform" } })
 	end,
+    },
+    {
+        "nvim-treesitter/nvim-treesitter-context",
+        dependency = { "nvim-treesitter/nvim-treesitter" },
+        config = function()
+            require("treesitter-context").setup({
+                enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+                max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+                min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+                line_numbers = true,
+                multiline_threshold = 20, -- Maximum number of lines to collapse for a single context line
+                trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+                mode = "cursor", -- Line used to calculate context. Choices: 'cursor', 'topline'
+                separator = nil, -- Separator between context and content. Should be a single character string, like '-'.
+                zindex = 20, -- The Z-index of the context window
+                on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
+            })
+        end,
+    }
 }
